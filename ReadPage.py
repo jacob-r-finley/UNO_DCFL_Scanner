66b56c98b7cf4cebd57048e11234210c8ee0af73
jacob-finley <jacobfinley@unomaha.edu>
2025-06-21 19:22:08 -0500

    Added refactored code to be more readable and allowed proper printing when debugging.

--- ReadPage.py
+++ ReadPage.py
@@ -7,20 +7,34 @@ from Logger import Logger
 # https://www.geeksforgeeks.org/python-web-scraping-tutorial/
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-all
 
+'''
+This is the Reader class that will read the UNO Police Reports page and return the data from today's log.
+It will use BeautifulSoup to parse the HTML and extract the relevant information.
+It will return a dictionary with the following keys:
+- offense: the offense reported
+- report: the report number
+- disposition: the disposition of the case
+- occurred: the date and time the offense occurred
+- building: the building where the offense occurred
+- location: the location of the offense
+- stolen: the value of stolen property
+- damaged: the value of damaged property
+- desc: a description of the offense
+'''
 class Reader:
     def __init__(self, link: str, l: Logger):
-        """Reader object designed to read from the UNODCL and return objects from todays log
+        '''Reader object designed to read from the UNODCL and return objects from todays log
 
         Args:
             link (str): the http link for the UNODCL
-        """
+        '''
         self.link = link
         self.request = self.testLink()
         self.soup = BeautifulSoup(self.request.content, 'html.parser')
         self.logger = l
-                
+
     def testLink(self):
-        """tests the link for UNOCDL
+        '''tests the link for UNOCDL
         
         Raises:
             ValueError: will return an error if webpage was unaccessible 
@@ -29,7 +43,7 @@ class Reader:
             False if webpage was unaccessible
             
             Response object if link was accessible
-        """
+        '''
         try:
             r = requests.get(self.link)
             status = r.status_code
@@ -39,15 +53,14 @@ class Reader:
             return False
         else:
             return r
-    
+
     def readPage(self) -> bool | dict:
-        """meat and potatoes of program. Reads the UNODCL and will splice the data accordingly
+        '''meat and potatoes of program. Reads the UNODCL and will splice the data accordingly
 
         Returns:
             bool: True if read was successful
-                
-                      False if read was unsuccessful
-        """
+                  False if read was unsuccessful
+        '''
         if not(self.link):
             return False
         i = str(self.logger.findNumberOfLogs())
