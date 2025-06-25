adda8337d9f959b135122a53fce8f891c079decc
jacob-r-finley <jacobryanfinley@gmail.com>
2025-06-25 10:47:15 -0500

    Added basic logging functionality, needs to be fleshed out.

--- Logger.py
+++ Logger.py
@@ -1,9 +1,33 @@
-from FormatDate import formatDate as FD
 import re
+import logging
+from FormatDate import formatDate as FD
 
 class Logger:
     def __init__(self, f):
         self.file = f
+        logger = logging.getLogger(__name__)  # Create a logger with the name of the current module
+        console_handler = logging.StreamHandler()  # Create a console handler to output logs to the console
+        file_handler = logging.FileHandler('test_logger_' + self.file, mode='a', encoding='utf-8')  # Create a file handler to output logs to a file
+        formatter = logging.Formatter(
+            '{asctime} - {levelname} - {message}',  # Define the format of the log messages
+            style='{',  # Use curly braces for formatting
+            datefmt='%Y-%m-%d %H:%M:%S'  # Define the date format for 'asctime'
+        )
+        console_handler.setFormatter(formatter)  # Set the formatter for the console handler
+        file_handler.setFormatter(formatter)  # Set the formatter for the file handler
+        logger.addHandler(console_handler)  # Add the console handler to the logger
+        logger.addHandler(file_handler)  # Add the file handler to the logger
+        logger.setLevel('INFO')  # Set the logging level to INFO
+        # logging.basicConfig(
+        #     level=logging.INFO, # Set the logging level so that anything at INFO level or higher is logged
+        #     style='{',
+        #     format='{asctime} - {levelname} - {message}',
+        #     datefmt='%Y-%m-%d %H:%M:%S', # Date format for 'asctime'
+        #     filename='test_logger_' + self.file,  # will need to modify this to use the actual file path
+        #     filemode='a',  # Append mode
+        #     encoding='utf-8',
+        # )
+        self.logger = logger  # Store the logger instance for later use
 
     def changeFile(self, f) -> None:
         '''
